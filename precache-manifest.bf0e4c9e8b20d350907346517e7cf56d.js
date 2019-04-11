4633238c75e16936790f72d5fed5be556202e3ea
Souravdey777 <piyush.kolkata@gmail.com>
2019-04-11 18:04:13 +0530

    Updates

---
+++ precache-manifest.bf0e4c9e8b20d350907346517e7cf56d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "3fdf20939e4acf15e003",
+    "url": "/weather-app/static/js/main.4e1db3b1.chunk.js"
+  },
+  {
+    "revision": "43a55c71f60142973e44",
+    "url": "/weather-app/static/js/2.0b69c989.chunk.js"
+  },
+  {
+    "revision": "3fdf20939e4acf15e003",
+    "url": "/weather-app/static/css/main.404af08a.chunk.css"
+  },
+  {
+    "revision": "0481181712bb6c55c4b0c82886d66c76",
+    "url": "/weather-app/index.html"
+  }
+];
